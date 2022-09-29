2bbfd9e88d055e7014a790bd4562a40edb850d90
melashu <meshu102@outlook.com>
2022-09-29 09:56:00 +0300

    dispatch to thunk action creator

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { bookActionCreator } from '../redux/books/Book';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addBookAsyncThunk } from "../redux/books/Book";
+import { ADD_BOOK as bookAction } from "../redux/books/Book";
 
 const AddBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [catagory, setCatagory] = useState("Business");
   const dispatch = useDispatch();
-  const bookList = useSelector((state) => state.book);
-  const ADD_BOOK = 'ADD_BOOK';
+  const bookList = useSelector((state) => state.book.books);
+
   return (
     <>
       <h1>Add Books</h1>
@@ -16,7 +17,12 @@ const AddBook = () => {
         onSubmit={(e) => {
           e.preventDefault();
           dispatch(
-            bookActionCreator(ADD_BOOK, { title, author, id: bookList.length }),
+            addBookAsyncThunk({
+              item_id: Math.random(100),
+              title: title,
+              author: author,
+              category: catagory,
+            })
           );
         }}
       >
@@ -34,6 +40,12 @@ const AddBook = () => {
             setAuthor(event.target.value);
           }}
         />
+        <select onChange={(e) => setCatagory(e.target.value)}>
+          <option value="Business">Business</option>
+          <option value="Fiction">Fiction</option>
+          <option value="Programming">Programming</option>
+          <option value="Self eelopment">Self delopment</option>
+        </select>
         <button type="submit">Add Book</button>
       </form>
     </>
